316007dcdccf48cca78a9bd9b5c6c8f36ffe3c2c
Arsen Anay <arsenanai@gmail.com>
2025-09-15 17:40:43 +0500

    adding version number

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,12 @@
   <router-view v-slot="{ Component }" class="">
     <component :is="Component" />
   </router-view>
+  <div
+    v-if="appVersion"
+    class="fixed bottom-2 left-2 text-[10px] md:text-xs text-gray-500 bg-white/70 dark:bg-gray-800/70 px-2 py-1 rounded shadow-sm select-none"
+  >
+    v{{ appVersion }}
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -9,6 +15,9 @@ import { ref, watch, onMounted } from "vue";
 import { useRouter, RouteLocationNormalized } from "vue-router";
 import { initFlowbite } from "flowbite";
 import { useAuthStore } from '@/stores/auth';
+
+// Hardcoded version label
+const appVersion = "1.0.1";
 
 // Initialize the router
 const router = useRouter();
