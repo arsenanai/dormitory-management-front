a2fdb70b56f974026803f7cbb29f44e46713ffeb
Arsen Anay <arsenanai@gmail.com>
2026-04-09 12:57:32 +0500

    updating the npm packages

--- vite.config.ts
+++ vite.config.ts
@@ -26,7 +26,12 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'http://localhost:8010',
+        changeOrigin: true,
+        secure: false,
+      },
+      '/storage': {
+        target: 'http://localhost:8010',
         changeOrigin: true,
         secure: false,
       },
